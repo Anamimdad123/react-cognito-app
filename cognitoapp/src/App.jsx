9b81e919d8704f5eaa7db95a2b5cabc34bc68a33
Anamimdad <144683993+Anamimdad@users.noreply.github.com>
2026-01-05 21:44:05 +0500

    Updated app.jsx

--- cognitoapp/src/App.jsx
+++ cognitoapp/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import './App.css';
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+import { fetchAuthSession } from 'aws-amplify/auth';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+Amplify.configure({ ...awsExports });
+let token = (await fetchAuthSession()).tokens?.idToken?.toString();
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+      <Authenticator>
+        {({ signOut, user }) => (
+            <div>
+              <p>Welcome {user.username}</p>
+                <p>Your token is: {token}</p>
+              <button onClick={signOut}>Sign out</button>
+            </div>
+        )}
+      </Authenticator>
+  );
 }
 
-export default App
+export default App;
