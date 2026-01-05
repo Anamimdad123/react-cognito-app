717145065501f99726f48cfc296da053b2b068a1
Anamimdad <144683993+Anamimdad@users.noreply.github.com>
2026-01-06 00:34:21 +0500

    added

---
+++ cognitoapp/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "reactcognito79d1f44f": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
