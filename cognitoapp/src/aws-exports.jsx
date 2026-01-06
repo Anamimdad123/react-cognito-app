d881af1bd5997153264af5224faf051411e19863
Anamimdad <144683993+Anamimdad@users.noreply.github.com>
2026-01-06 15:48:13 +0500

    updated

--- cognitoapp/src/aws-exports.jsx
+++ cognitoapp/src/aws-exports.jsx
@@ -2,8 +2,8 @@
 const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "us-east-1_kVQkz8PMg",
-    "aws_user_pools_web_client_id": "4uee4o0nimtfmv8uptigpaolq4"
+    "aws_user_pools_id": "us-east-1_qJjvGiIRx",
+    "aws_user_pools_web_client_id": "22lbtbntihapcrnof2sosd4b3l"
 }
 
 export default awsmobile;
